fef500c4b620f8125454a72644f91a8f3d4246f8
Yao Hao Lai <ylai@student.42signapore.sg>
2025-04-25 15:32:34 +0800

    feat(ex00): finished ex00

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -9,13 +9,25 @@ class ClapTrap
     std::string name;
     int hitPoints;
     int energyPoints;
-    int attack;
+    int attackDamage;
 
   public:
+    // Orthodox Canonical Form
     ClapTrap();
+    ClapTrap(std::string name);
     ClapTrap(const ClapTrap &other);
     ClapTrap &operator=(const ClapTrap &other);
     ~ClapTrap();
+    // Getters and Setters
+    std::string getName() const;
+    void setName(std::string name);
+    int getHitPoints() const;
+    void setHitPoints(int newHitPoints);
+    int getEnergyPoints() const;
+    void setEnergyPoints(int newEnergyPoints);
+    int getAttackDamage() const;
+    void setAttackDamage(int newAttackDamage);
+
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
